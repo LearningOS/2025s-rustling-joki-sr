4daf01a9215a1e5c170c1750c7d66144569e3542
joki-sr <115058227+joki-sr@users.noreply.github.com>
2025-04-01 08:53:51 +0000

    update

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,8 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+#[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
@@ -14,8 +13,11 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // borrow rather than move
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
+    // println!("dbg:  {:?}",y);
     y; // Fix without deleting this line.
+
 }
